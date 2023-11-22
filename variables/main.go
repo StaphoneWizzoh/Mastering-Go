c71002256312b8d4565ae47fc1ec577af530b703
Staphone <staphoneomondi@gmail.com>
2023-11-22 18:02:31 +0300

    modified:   main.go

--- variables/main.go
+++ variables/main.go
@@ -1,13 +1,17 @@
 package main
 
-import "fmt"
-
-var (
-	i, j int16 = 42,60
+import (
+	"fmt"
+	"strconv"
 )
 
+
+
 func main () {
-	index := 42.0
-	fmt.Printf("%v, %T \n",index, index)
-	fmt.Printf("%v, %T \n",i, i)
+	var i int = 50
+	fmt.Printf("%v, %T\n", i, i)
+
+	var j string
+	j = strconv.Itoa(i)
+	fmt.Printf("%v, %T\n", j, j)
 }
