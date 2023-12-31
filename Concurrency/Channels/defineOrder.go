49a5a5048df78e3a38e928bbd89f8fcfbbc3f294
Staphone <staphoneomondi@gmail.com>
2023-12-31 22:50:11 +0300

    Concurrency: Advanced

---
+++ Concurrency/Channels/defineOrder.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func A(a, b chan struct{}){
+	<-a
+	fmt.Println("A()!")
+	time.Sleep(time.Second)
+	close(b)
+}
+
+// The function is blocked until channel a is closed.
+func B(a, b chan struct{}){
+	<-a
+	fmt.Println("B()!")
+	close(b)
+}
+
+func C(a chan struct{}){
+	<-a
+	fmt.Println("C()!")
+}
+
+func main(){
+	x := make(chan struct{})
+	y := make(chan struct{})
+	z := make(chan struct{})
+
+	go C(z)
+	go A(x, y)
+	go C(z)
+	go B(y,z)
+	go C(z)
+
+	close(x)
+	time.Sleep(3 * time.Second)
+}
